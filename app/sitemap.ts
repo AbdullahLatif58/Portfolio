6a564a18896f696bb0a7769c3b1a2a728adcc795
webs <22011556-068@uog.edu.pk>
2026-08-09 15:32:41 +0500

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,7 +14,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${baseUrl}/projects`,
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.8,
+      priority: 9,
     },
   ];
 }
